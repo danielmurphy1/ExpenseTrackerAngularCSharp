90d6a147134b387222059bf830640da39590290a
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-09-14 22:20:08 -0400

    changed DatabaseConnection class to use updated Npgsql usage. Added GetExpensesService to send data to ExpensesController. Updated ExpensesController to return data from database instead of hardcoded data

--- DatabaseConnection.cs
+++ DatabaseConnection.cs
@@ -5,17 +5,11 @@ namespace ExpenseTrackerAngularCSharp
 {
     public class DatabaseConnection
     {
-        public NpgsqlConnection GetConnection()
+        public NpgsqlDataSource GetConnection()
         {
             Env.TraversePath().Load();
             string connString = Environment.GetEnvironmentVariable("DEVELOPMENT_CONNECTION_STRING")!;
-            if (string.IsNullOrEmpty(connString))
-            {
-                throw new Exception("DB Connection String is Null");
-            }
-            NpgsqlConnection conn = new NpgsqlConnection(connString);
-            conn.Open();
-            return conn;
+            return NpgsqlDataSource.Create(connString);
         }
     }
 }
---
+++ ExpensesDatabaseServices/GetExpensesService.cs
@@ -0,0 +1,40 @@
+﻿using DotNetEnv;
+using ExpenseTrackerAngularCSharp.Models;
+using Npgsql;
+using System.Data.Common;
+
+namespace ExpenseTrackerAngularCSharp.ExpensesDatabaseServices
+{
+    public class GetExpensesService
+    {
+        public async Task<List<ExpenseModel>> GetExpenses()
+        {
+            DatabaseConnection dbconnection = new DatabaseConnection();
+            await using (NpgsqlDataSource dataSource = dbconnection.GetConnection())
+            {
+                List<ExpenseModel> expenses = new List<ExpenseModel>();
+                await using (NpgsqlCommand command = dataSource.CreateCommand("SELECT * FROM expenses ORDER BY id"))
+                {
+                    await using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            ExpenseModel expense = new ExpenseModel
+                            {
+                                Id = reader.GetInt32(0),
+                                Date = reader.GetString(1),
+                                Location = reader.GetString(2),
+                                Description = reader.GetString(3),
+                                Amount = reader.GetDecimal(4)
+                            };
+                            expenses.Add(expense);
+                        }
+
+                        return expenses;
+                    };
+                };
+            };
+            
+        }
+    }
+}
